cbe92aa1669251e5865c834acf41b7803f0cf663
Luis Eduardo González Téllez <electrongy@hotmail.com>
2016-10-25 18:38:33 -0500

    Se crearon las capas de lógica de negocios (BLL) y de acceso a datos (DAL), así como las entidades necesarias para el manejo de la información.

---
+++ FarmaciaMagaña.DAL/VentasDAL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FarmaciaMagaña.Entities;
+
+namespace FarmaciaMagaña.DAL
+{
+    class VentasDAL
+    {
+
+        public VentasDAL()
+        {
+
+        }
+
+        public List<Ventas> getAllVentas()
+        {
+
+        } 
+
+        public Ventas getVenta(int idVenta)
+        {
+
+           
+        }
+
+        public Boolean createVenta(Ventas venta)
+        {
+
+            return true;
+        }
+
+        public Boolean deleteVenta(int idVenta)
+        {
+
+            return true;
+        }
+    }
+}
---
+++ FarmaciaMagañaBLL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FarmaciaMagañaBLL.MvcApplication" Language="C#" %>
